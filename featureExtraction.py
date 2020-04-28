fd7e0eb251f141759ed4534d3a633553d4977d57
ClownMonster <mk6386223@gmail.com>
2020-04-28 09:08:56 +0530

    reshape done

---
+++ featureExtraction.py
@@ -0,0 +1,8 @@
+'''
+ Extract only the feature that should be supplied to neural network 
+ using keras library which is an high level api of tensor flow for building and trainig deep 
+ learning models
+
+'''
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout
